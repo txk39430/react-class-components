430345eb313a5b0f8709ade09c7242be9049f417
tejaswinikolluru <txk39430@ucmo.edu>
2026-04-10 11:26:29 -0600

    Fix submodule issue

---
+++ new-app/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>First Element</h1>
+        <h1>Scecond Element </h1>
+
+      </>
+
+
+    );
+  }
+}
+
+export function App2() {
+  return (
+    <>
+      <h1>Learning never ends </h1>
+    </>
+  );
+}
+
+//import React from 'react';
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <h1>App Component</h1>
+//       </>
+//     );
+//   }
+// }
+
---
+++ new-app/src/User.js
@@ -0,0 +1,9 @@
+function User() {
+  return (
+    <div className="User">
+   <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default User;
